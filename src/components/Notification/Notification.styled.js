55153c9d10c6ce35e64ad2fcbc32df381db71eef
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2023-08-27 22:30:31 +0300

    fix

--- src/components/Notification/Notification.styled.js
+++ src/components/Notification/Notification.styled.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const StyledNotification = styled.div`
+  display: flex;
+  justify-content: center;
   background-color: #ffebeb;
   color: #d32f2f;
   padding: 10px;
